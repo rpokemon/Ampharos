a08e01b8d3c97bc22f44453cf39dea2a4fe48600
Josh B <josh.ja.butt@gmail.com>
2019-11-24 23:13:36 +1000

    Fix gen VIII abilities

--- ampharos/__init__.py
+++ ampharos/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'ampharos'
 __author__ = 'Bijij'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Bijij'
-__version__ = '1.1.1a'
+__version__ = '1.1.2a'
 
 from . import tables
 from .search import *
