3ebf2217d6916953434d872e140cd8478874bc7b
Josh <josh.ja.butt@gmail.com>
2021-08-03 19:47:28 +1000

    Ampharos v3

--- ampharos/__init__.py
+++ ampharos/__init__.py
@@ -2,7 +2,7 @@ __title__ = "ampharos"
 __author__ = "Bijij"
 __license__ = "MIT"
 __copyright__ = "Copyright 2019-present Bijij"
-__version__ = "2.0.1"
+__version__ = "3.0.0"
 
 from . import tables as tables
 from .search import *
--- ampharos/setup.py
+++ ampharos/setup.py
@@ -2,8 +2,7 @@ import json
 
 from collections import namedtuple
 from typing import Any, List, Tuple
-
-from donphan import MaybeAcquire
+import asyncpg
 
 from .tables import ALL_TABLES
 from .utils import get_base_dir
@@ -12,29 +11,27 @@ from .utils import get_base_dir
 BASE_DIR = get_base_dir()
 
 
-async def setup_ampharos():
+async def setup_ampharos(connection: asyncpg.Connection):
     """Populates the Pokemon database.
 
     This method should always be called on startup"""
-    async with MaybeAcquire() as connection:
+    # Populate the tables if required
+    for table in ALL_TABLES:
 
-        # Populate the tables if required
-        for table in ALL_TABLES:
+        # If Table is empty
+        if (await table.fetch_row(connection)) is None:
 
-            # If Table is empty
-            if (await table.fetch_row(connection)) is None:
+            record = namedtuple("record", (column.name for column in table._columns))
+            data: List[Tuple[Any, ...]] = []
 
-                record = namedtuple("record", (column.name for column in table._columns))
-                data: List[Tuple[Any, ...]] = []
-
+            try:
+                with open(BASE_DIR / f"data/{table.__name__.lower()}.json") as f:
+                    for item in json.load(f):
+                        data.append(record(**item))
+            except FileNotFoundError:
+                print(f"Could not find Pokemon data file {table.__name__.lower()}.json")
+            else:
                 try:
-                    with open(BASE_DIR / f"data/{table.__name__.lower()}.json") as f:
-                        for item in json.load(f):
-                            data.append(record(**item))
-                except FileNotFoundError:
-                    print(f"Could not find Pokemon data file {table.__name__.lower()}.json")
-                else:
-                    try:
-                        await table.insert_many(connection, table._columns, *data)
-                    except Exception as e:
-                        print(e)
+                    await table.insert_many(connection, table._columns, *data)
+                except Exception as e:
+                    print(e)
